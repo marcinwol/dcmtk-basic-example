567eb4d0727e9dca1780949a53c6a5a399f7e640
marcinwol <wolskint@o2.pl>
2015-06-22 17:21:33 +0800

    main.cpp much simplified.

--- main.cpp
+++ main.cpp
@@ -1,16 +1,13 @@
 /**
- * The example program just takes one DCM image
- * and converts it into tiff image.
+ * The basic example program just takes one DCM image and reads a patient's name from it.
+ *
+ * For more information go to: https://github.com/marcinwol/dcmtk-basic-example
  */
 
 #include <iostream>
-#include <memory>
 
-#include "dcmtk/config/osconfig.h"
 #include "dcmtk/dcmdata/dctk.h"
 #include "dcmtk/dcmimgle/dcmimage.h"
-#include "dcmtk/dcmimage/dipitiff.h"
-
 
 using namespace std;
 
@@ -18,15 +15,18 @@ int main() {
 
     string root_path;
 
+    // SOURCE_CODE_LOCATION is set by cmake during compilation.
+    // this contains the path to the source folder.
+    // This is only helper for convenience. Without this
+    // in_file below should contain absolute path.
     #ifdef SOURCE_CODE_LOCATION
         root_path = SOURCE_CODE_LOCATION;
     #endif
 
     string in_file {root_path  + "/DCMIMAGES/77654033/20010101/CR1/6154"};
-    string out_file {root_path + "/DCMIMAGES/6154.tiff"};
 
-    DcmFileFormat fileformat;
-    OFCondition status = fileformat.loadFile(in_file.c_str());
+    DcmFileFormat file_format;
+    OFCondition status = file_format.loadFile(in_file.c_str());
 
     if (status.bad()) {
         cout << "Problem openning file:" << in_file << endl;
@@ -35,35 +35,15 @@ int main() {
         cout << "DCM file opened properly" << endl;
     }
 
-    DcmDataset* dataset = fileformat.getDataset();
-    E_TransferSyntax xfer = dataset->getOriginalXfer();
+    DcmDataset* dataset = file_format.getDataset();
 
-    // create a DicomImage object
-    shared_ptr<DicomImage> dcm_img {new DicomImage(dataset, xfer)};
+    OFString patient_name;
 
-    if (dcm_img->getStatus() == EIS_InvalidImage)
-    {
-        cout << "Failed to open: " << in_file << endl;
-        return 1;
-    }
+    dataset->findAndGetOFStringArray(DCM_PatientName, patient_name);
 
-    // make a tiff image
-    FILE* ofile = nullptr;
-    ofile = fopen(out_file.c_str(), "wb");
-
-    DiTIFFPlugin tiffPlugin;
-    tiffPlugin.setCompressionType(E_tiffLZWCompression);
-    tiffPlugin.setLZWPredictor(E_tiffLZWPredictorDefault);
-    tiffPlugin.setRowsPerStrip(0);
-
-    int result = dcm_img->writePluginFormat(&tiffPlugin, ofile, 0);
-
-    fclose(ofile);
-    
-    cout << "Tiff written as: " << out_file << endl;
+    cout << "Patient name is: " << patient_name << endl;
 
     cout << "Successful execution" << endl;
-
     return 0;
 }
 
